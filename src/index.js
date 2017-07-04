555ca8afecb3ae3ef02c793660c2fdc852d3a9af
rain <niraj_ads@yahoo.com>
2017-07-04 15:30:50 +0400

    script-fix

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ export default function StreamFile(options) {
           self.push(createFile(`${language}/${templateName}`, templateVale))
         })
       } else {
-        self.push(createFile(`${language}.json`, JSON.stringify(value)))
+        self.push(createFile(`${language}.json`, JSON.stringify(value,null,'\n')))
       }
     });
 
@@ -78,4 +78,5 @@ export default function StreamFile(options) {
   loadSpreadsheet(options, stream);
   return stream.pipe(vfs.dest(options.path));
 }
+
 
